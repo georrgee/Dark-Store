d83557db946d520645a971866a832c055cb9ff8a
George Garcia <garcia.george.b@gmail.com>
2019-06-04 20:00:44 -0700

    - Created SearchResultCell (collectionViewCell) - added three labels: (app name, category, and rating) - added imageView - added Get Button - added constraints along with stackViews

---
+++ Dark Store/Controller/AppsSearchController.swift
@@ -0,0 +1,44 @@
+//  AppsSearchController.swift
+//  Dark Store
+
+//  Created by George Garcia on 6/3/19.
+//  Copyright © 2019 GeeTeam. All rights reserved.
+
+import UIKit
+
+class AppsSearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    fileprivate let cellID = "1234"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .white
+        
+        collectionView.register(SearchResultCell.self, forCellWithReuseIdentifier: cellID)
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        //return CGSize(width: <#T##CGFloat#>, height: <#T##CGFloat#>)
+        return .init(width: view.frame.width, height: 250)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath)
+                
+        return cell
+    }
+    
+    init() {
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ Dark Store/Controller/MainTabBarController.swift
@@ -0,0 +1,71 @@
+//  MainTabBarController.swift
+//  Dark Store
+
+//  Created by George Garcia on 6/2/19.
+//  Copyright © 2019 GeeTeam. All rights reserved.
+
+import UIKit
+
+class MainTabBarController: UITabBarController { // UITabBarController -> Super Class
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBar.barTintColor = .black
+        tabBar.tintColor = .white
+        
+        viewControllers = [ // creating viewcontrollers for 3 tabs
+            createNavController(viewController: AppsSearchController(), title: "Search", imageName: "search"),
+            createNavController(viewController: UIViewController(), title: "Today", imageName: "today"),
+            createNavController(viewController: UIViewController(), title: "Apps", imageName: "sheets")
+        ]
+    }
+    
+    fileprivate func createNavController(viewController: UIViewController, title: String, imageName: String) -> UIViewController {
+        
+        let navigationController = UINavigationController(rootViewController: viewController)
+        navigationController.navigationBar.prefersLargeTitles = true
+        navigationController.tabBarItem.title = title
+        navigationController.tabBarItem.image = UIImage(named: imageName)
+        
+        viewController.view.backgroundColor   = .white
+        viewController.navigationItem.title   = title
+        
+        return navigationController
+    }
+}
+
+
+
+/*   MARK - NOTES
+ 
+ 1) Before Adding refactored code. This was in the viewDidLoad Method
+ 
+ //        let todayController = UIViewController()
+ //        todayController.navigationItem.title = "Today"
+ //        todayController.view.backgroundColor = .white
+ //
+ //        let todayNavController = UINavigationController(rootViewController: todayController)
+ //        todayNavController.tabBarItem.title = "Today"
+ //        todayNavController.tabBarItem.image = #imageLiteral(resourceName: "today")
+ //        todayNavController.navigationBar.prefersLargeTitles = true
+ //
+ //        let redViewController = UIViewController()
+ //        redViewController.navigationItem.title = "Apps"
+ //        redViewController.view.backgroundColor = .white
+ //
+ //        let redNavController = UINavigationController(rootViewController: redViewController)
+ //        redNavController.tabBarItem.title = "Apps"
+ //        redNavController.tabBarItem.image = #imageLiteral(resourceName: "sheets")
+ //        redNavController.navigationBar.prefersLargeTitles = true
+ //
+ //        let blueViewController = UIViewController()
+ //        blueViewController.navigationItem.title = "Search"
+ //        blueViewController.view.backgroundColor = .white
+ //
+ //        let blueNavController = UINavigationController(rootViewController: blueViewController)
+ //        blueNavController.tabBarItem.title = "Search"
+ //        blueNavController.tabBarItem.image = UIImage(named: "search")
+ //        blueNavController.navigationBar.prefersLargeTitles = true
+ 
+ */
